dc4bfe5d2955ded31e55c9d48873c6fce43bbdb6
Roxane Noël <roxane.noel92@gmail.com>
2023-05-13 15:51:34 +0100

    Added code to serve index page

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  // Home page
+  if(req.url === '/') {
+    fs.readFile('index.html', function(err, data){
+      if (err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end;
+    });
+  }
+  
+
 }).listen(8080);
