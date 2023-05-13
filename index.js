6bedd58a89f847bc8b4e99de831b8cb348e292b0
Roxane Noël <roxane.noel92@gmail.com>
2023-05-13 16:07:31 +0100

    Added code to serve about, contact, 404 pages

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ http.createServer(function (req, res) {
       return res.end;
     });
   }
-  
+  // About page
+  else if (req.url === '/about') {
+    fs.readFile('about.html', function(err, data){
+      if (err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end;
+    });
+  }
+  // Contact page
+  else if (req.url === '/contact-me') {
+    fs.readFile('contact.html', function(err, data){
+      if (err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end;
+    });
+  } 
+  // 404
+  else {
+    fs.readFile('404.html', function(err, data){
+      if (err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end;
+    });
+  }
 
 }).listen(8080);
